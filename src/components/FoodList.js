581eb2848482b093b0f0353e36484398152e84ee
Dayeon Lee <iamdayeonlee@gmail.com>
2024-08-21 20:16:16 +0100

    Add english name for food title

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,13 +1,16 @@
-function FoodListItem({ item }) {
+import englishTitle from "../enTitle";
+
+function FoodListItem({ item, enTitle }) {
   const dateFormat = () => {
     const timeStamp = new Date(item.updatedAt);
-    const formatDate = timeStamp.toISOString().substring(0, 10);
+    // const formatDate = timeStamp.toISOString().substring(0, 10);
+    const formatDate = timeStamp.toUTCString().substring(0, 11);
     return formatDate;
   };
   return (
     <>
       <div
-        className="flex h-[138px] w-[400px] gap-5 border-2 border-grey rounded-xl p-4 my-2 w-[506px]"
+        className="flex items-center h-[150px] w-[400px] gap-5 border-2 border-grey rounded-xl p-4 my-2 w-[506px]"
         key={item.id}
       >
         <div className>
@@ -23,6 +26,7 @@ function FoodListItem({ item }) {
             <span className="bg-grey p-1 m-2 rounded text-darkgreen font-extrabold text-sm">
               {item.calorie} kcal
             </span>
+            <p className="font-semibold text-lg">{enTitle}</p>
           </div>
           <div className="text-gray-500">{item.content}</div>
           <div className="text-gray-400 "> {dateFormat(item.createdAt)}</div>
@@ -33,15 +37,17 @@ function FoodListItem({ item }) {
 }
 
 function FoodList({ items }) {
-  return (
-    <ul className="flex flex-wrap justify-start gap-3">
-      {items.map((item) => (
-        <li key={item.id}>
-          <FoodListItem item={item} />
-        </li>
-      ))}
-    </ul>
-  );
+  const itemsList = items.map((item) => {
+    const enTitleFind = englishTitle.find((enItem) => item.id === enItem.id);
+    const enTitle = enTitleFind?.title;
+    return (
+      <li key={item.id}>
+        <FoodListItem item={item} enTitle={enTitle} />
+      </li>
+    );
+  });
+
+  return <ul className="flex flex-wrap justify-start gap-3">{itemsList}</ul>;
 }
 
 export default FoodList;
